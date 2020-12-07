a7b6453d0fc18bcb500b0f99efe5ba2642e4e663
BaraaAlastal <70208732+BaraaAlastal@users.noreply.github.com>
2020-12-07 20:45:23 +0200

    Update index.js

--- index.js
+++ index.js
@@ -80,52 +80,32 @@ var allData = JSON.parse(JSON.stringify(contentJsonFile));
 function batterTypes(){
 	var allBatterTypes =[];
 	var batterArrays =[];
-
 	for ( var i=0 ; i<allData.length ; i++) {
-
 		batterArrays = allData[i].batters.batter;
-
-
-        for (var batterElement of batterArrays) {
+		for (var batterElement of batterArrays) {
 		     allBatterTypes.push(batterElement.type);
-
 		}
-		
-		}
-		var x = document.getElementById("batter").innerHTML=allBatterTypes;
-    return x;
-    
+	}
+	var x = document.getElementById("batter").innerHTML=allBatterTypes;
+    	return x;
 }
 
 function toppingTypes(){
 	var allToppingTypes =[];
 	var toppingArrays =[];
-
-	  for ( var i=0 ; i<allData.length ; i++) {
-          
-          toppingArrays = allData[i].topping;
-
-
-        for (var toppingElement of toppingArrays) {
-		  
-          allToppingTypes.push(toppingElement.type);
-
-	}
-
+	for ( var i=0 ; i<allData.length ; i++) {
+          	toppingArrays = allData[i].topping;
+		for (var toppingElement of toppingArrays) {
+		  allToppingTypes.push(toppingElement.type);
+		}
 	}
 	var x = document.getElementById("topping").innerHTML=allToppingTypes;
-    return x;
-
+        return x;
 } 
 
-
 function ppuAverage(){
-
-
 	var Average =0;
-
 	for ( var element of allData) {
-
 		Average += element.ppu;
 	}
     var x = document.getElementById("ppu avg").innerHTML=Average/allData.length;
@@ -133,9 +113,7 @@ function ppuAverage(){
 }
 
 function ppuSum(){
-
 	var Sum =0;
-
 	for ( var element of allData) {
 		Sum += element.ppu;
 	}
@@ -143,29 +121,19 @@ function ppuSum(){
     return x;
 }
 
-
 function allIds(){
-
-  var allIds = [];
-
-	  for ( var i=0 ; i<allData.length ; i++) {
-
-      
-       	batterArrays = allData[i].batters.batter;
-
-
-        for (var batterElement of batterArrays) {
-		     allIds.push(batterElement.id);}
-
-
-        toppingArrays = allData[i].topping;
-         for (var toppingElement of toppingArrays) {
-		  
-          allIds.push(toppingElement.id);}
+  	var allIds = [];
+	for ( var i=0 ; i<allData.length ; i++) {
+		batterArrays = allData[i].batters.batter;
+		for (var batterElement of batterArrays) {
+		     allIds.push(batterElement.id);
+		}
+        	toppingArrays = allData[i].topping;
+         	for (var toppingElement of toppingArrays) {	  
+          		allIds.push(toppingElement.id);}
 		}       
-		var x = document.getElementById("IDs").innerHTML=allIds;
-		return x;
-
+	var x = document.getElementById("IDs").innerHTML=allIds;
+	return x;
 }
 // i call these below functions so that show results in html document   
 toppingTypes();
